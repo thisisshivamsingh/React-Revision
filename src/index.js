4b1803e0ba7d2966c39f0019db238e696a8c3b63
Shivam <thisisshivam06@gmail.com>
2022-07-31 16:46:10 +0530

    Mini Book Project, Props, Props-Destructuring and Props-Children

--- src/index.js
+++ src/index.js
@@ -1,40 +1,85 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// stateless functional component
-// always return JSX
-
-// JSX Rules
-// return single element
-// div / section / article or fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-// Nested Components, React Tools
-
-function Greeting() {
+// CSS
+import "./index.css";
+// setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL480_FMwebp_QL65_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71yEL+oXg+L._AC_UY327_FMwebp_QL65_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen",
+};
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          non et illo minus sunt cumque magnam in sed, natus ipsa.
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
   );
 }
-
-const Person = () => <h2>john doe</h2>;
-
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = (props) => {
+  const { img, title, author, children } = props;
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  );
 };
+// Nested Components, React Tools
 
-// const Greeting = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "hello world")
+// function BookList() {
+//   return (
+//     <section>
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
+//     </section>
+//   );
+// }
+
+// const Book = () => {
+//   return (
+//     <article>
+//       <Image></Image>
+//       <Title />
+//       <Author />
+//     </article>
 //   );
 // };
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+// const Image = () => (
+//   <img
+//     src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL480_FMwebp_QL65_.jpg"
+//     alt=""
+//   />
+// );
+
+// const Title = () => <h1>I Love to the Moon and Black</h1>;
+// const Author = () => <h4>Amelia Hepworth</h4>;
+ReactDom.render(<BookList />, document.getElementById("root"));
